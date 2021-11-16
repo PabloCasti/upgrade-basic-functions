d215586aad0a73278a4fd556fb0a47000bae97f4
PabloCas <MadridCastilla@hotmail.com>
2021-11-16 07:33:19 +0100

    Iteration#2

---
+++ Iteration2.js
@@ -0,0 +1,22 @@
+// 2. Buscar la palabra más larga
+
+const avengers = [
+  "Hulk",
+  "Thor",
+  "IronMan",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+function findLongestWord(param) {
+  let longestWord = param[0];
+
+  for (let i = 0; i < longestWord.length; i++) {
+    if (param[i] && param[i].length > longestWord.length) {
+      longestWord = param[i];
+    }
+  }
+  console.log(findLongestWord);
+}
+
+findLongestWord(avengers);
