e23dfa8deb4eb24b72fe51c9b86b128e17c09690
PabloCas <MadridCastilla@hotmail.com>
2021-11-16 08:04:53 +0100

    Iteration#8

---
+++ Iteration8.js
@@ -0,0 +1,26 @@
+// 8. Contador de repeticiones
+
+const counterWords = [
+  "code",
+  "repeat",
+  "eat",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+  "enjoy",
+  "upgrade",
+  "code",
+];
+function repeatCounter(param) {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] in count) {
+      count[array[i]]++;
+    } else {
+      count[array[i]] = 1;
+    }
+  }
+  return count;
+}
