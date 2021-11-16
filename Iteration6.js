decb9c97980b6f2f70cb7739e4f619e97fb894ed
PabloCas <MadridCastilla@hotmail.com>
2021-11-16 07:46:24 +0100

    Iteration#6

---
+++ Iteration6.js
@@ -0,0 +1,26 @@
+// 6. Valores Únicos
+
+const duplicates = [
+  "sushi",
+  "pizza",
+  "burger",
+  "potato",
+  "pasta",
+  "ice-cream",
+  "pizza",
+  "chicken",
+  "onion rings",
+  "pasta",
+  "soda",
+];
+function removeDuplicates(array) {
+  let newElements = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!newElements.includes(array[i])) {
+      newElements.push(array[i]);
+    }
+  }
+  return newElements;
+}
+
+console.log(removeDuplicates(duplicates));
